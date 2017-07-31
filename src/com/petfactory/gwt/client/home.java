fae5c23cd5eff16f9a4ca72fcaaddeb3b10a624e
Daniel Santos <dfsantosbu@unal.edu.co>
2017-07-31 18:49:32 -0500

    Add first version project with an entry point, a module and an index.

---
+++ src/com/petfactory/gwt/client/home.java
@@ -0,0 +1,13 @@
+package com.petfactory.gwt.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class home implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
