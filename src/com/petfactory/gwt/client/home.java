2d7c713f6ba07af6f7d6d8d86dedd206c9958398
Daniel Santos <dfsantosbu@unal.edu.co>
2017-07-31 20:04:39 -0500

    Add working version of the application.

--- src/com/petfactory/gwt/client/home.java
+++ src/com/petfactory/gwt/client/home.java
@@ -1,13 +1,28 @@
 package com.petfactory.gwt.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class home implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		// TODO Auto-generated method stub
+		// Create a Label and an HTML widget.
+	    Label lbl = new Label("This is just text.  It will not be interpreted "
+	      + "as <html>.");
 
+	    HTML html = new HTML(
+	      "This is <b>HTML</b>.  It will be interpreted as such if you specify "
+	        + "the <span style='font-family:fixed'>asHTML</span> flag.", true);
+
+	    // Add them to the root panel.
+	    VerticalPanel panel = new VerticalPanel();
+	    panel.add(lbl);
+	    panel.add(html);
+	    RootPanel.get().add(panel);
 	}
 
 }
